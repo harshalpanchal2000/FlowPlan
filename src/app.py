60f8ce62bd4ea2fa1ee291882f9a623241c5d61f
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-17 23:44:52 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,17 +1,14 @@
 import streamlit as st
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
 
 # Title and Description
 st.title("FlowPlan: Seamlessly Navigate Your Day, Every Day.")
 st.write("Too much to do? Too little time? Meet FlowPlan: Your AI task-scheduling assistant. Get a personalized schedule daily, effortlessly. FlowPlan takes your tasks, prioritizes, and optimizes your schedule automatically. Perfect plans, always.")
 
 # Collecting User Input
-st.header("Input your daily tasks and goals:")
-
-user_input = st.text_area("Please list your daily tasks and goals here. For example:\n1. 8:00 AM - Breakfast\n2. 9:00 AM - Work on project X\n3. 12:30 PM - Lunch\n4. 3:00 PM - Gym session")
-
-import streamlit as st
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
-from threading import Thread
+#st.header("Input your daily tasks and goals:")
+#user_input = st.text_area("Please list your daily tasks and goals here. For example:\n1. 8:00 AM - Breakfast\n2. 9:00 AM - Work on project X\n3. 12:30 PM - Lunch\n4. 3:00 PM - Gym session")
 
 # Load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("google/codegemma-7b-it")
