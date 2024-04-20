20d094408def21c49f4533bfa8af7683ed0292c2
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-20 00:21:23 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,21 +1,6 @@
 import streamlit as st
 from transformers import pipeline, Conversation
 
-page_bg_img = """
-<style>
-[data-testid="stApp"] {
-    background-image: url('https://github.com/harshalpanchal2000/FlowPlan/blob/main/bg_image.png');
-    background-repeat: no-repeat;
-    background-size: cover; /* Adjust the background size as needed */
-    background-position: center; /* Adjust the background position as needed */
-}
-</style>
-"""
-
-# Apply the custom CSS style to set the background image
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
 # Title and Description
 st.title("FlowPlan: Seamlessly Navigate Your Day, Every Day.")
 st.write("Too much to do? Too little time? Meet FlowPlan: Your AI task-scheduling assistant. Get a personalized schedule daily, effortlessly. FlowPlan takes your tasks, prioritizes, and optimizes your schedule automatically. Perfect plans, always.")
@@ -29,7 +14,11 @@ def flowplan(message: str) -> str:
     Returns:
         str: The generated response.
     """
-    flowplan_pipeline = pipeline("text-generation", model="google/codegemma-7b-it")
+    model_path = "https://github.com/your_username/your_repository/raw/main/model-00001-of-00004.safetensors"  # Replace with the Git LFS URL to your model
+    config_path = "https://github.com/your_username/your_repository/raw/main/config.json"  # Replace with the Git LFS URL to your model configuration file
+
+    # Generate response
+    flowplan_pipeline = pipeline("text-generation", model=model_path)
     conversation = Conversation()
     conversation.add_user_input(message)
     response = flowplan_pipeline(conversation)
